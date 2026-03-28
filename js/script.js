1583e748ed9436317edbc27550ef552599a3cedb
TilenCecko <167509679+TilenCecko@users.noreply.github.com>
2026-03-28 13:42:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ var f = 0;
 var ctx;
 var intervalId;
 var timerId;
+var level = 1;
 
-var paddlecolor = "#000000";
-var ballcolor = "#333333";
+var paddlecolor = "#0f2742";
+var ballcolor = "#fb8500";
 var brickcolors = {
     1: "#7cc576",
     2: "#f5a623",
@@ -35,16 +36,31 @@ var NCOLS;
 var BRICKWIDTH;
 var BRICKHEIGHT;
 var PADDING;
+var gameWon = false;
+
+function updateLevelDisplay() {
+    $("#level").html(level);
+}
+
+function resetBall() {
+    x = 150;
+    y = 200;
+    dx = 2;
+    dy = 4;
+    start = true;
+}
 
 function init() {
     ctx = $("#canvas")[0].getContext("2d");
     WIDTH = $("#canvas").width();
     HEIGHT = $("#canvas").height();
+    resetBall();
     init_paddle();
     initbricks();
     sekunde = 0;
     izpisTimer = "00:00";
     tocke = 0;
+    updateLevelDisplay();
     $("#cas").html(izpisTimer);
     $("#tocke").html(tocke);
     intervalId = setInterval(draw, 10);
@@ -54,16 +70,40 @@ function init() {
 function init_paddle() {
     paddlex = WIDTH / 2;
     paddleh = 10;
-    paddlew = 100;
+
+    if(level==1){
+        paddlew = 150;
+    }else if(level==2){
+        paddlew = 100;
+    }else if(level==3){
+        paddlew = 50;
+    }else{
+        paddlew = 150;
+    }
 }
 
 function initbricks() {
-    NROWS = 5;
-    NCOLS = 5;
+    NROWS = 2;
+    NCOLS = 3;
     BRICKWIDTH = (WIDTH / NCOLS) - 1;
     BRICKHEIGHT = 30;
     PADDING = 1;
     bricks = new Array(NROWS);
+
+
+    if(level==1){
+        NROWS = 2;
+        NCOLS = 3;
+    }else if(level==2){
+        NROWS = 5;
+        NCOLS = 5;
+    }else if(level==3){
+        NROWS = 10;
+        NCOLS = 10;
+    }else{
+        NROWS = 2;
+        NCOLS = 2;
+    }
 
     for (var i = 0; i < NROWS; i++) {
         bricks[i] = new Array(NCOLS);
@@ -89,6 +129,18 @@ function rect(x, y, w, h) {
 
 function clear() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function allBricksDestroyed() {
+    for (var i = 0; i < NROWS; i++) {
+        for (var j = 0; j < NCOLS; j++) {
+            if (bricks[i][j] > 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 function timer() {
@@ -123,10 +175,33 @@ function onKeyUp(evt) {
     }
 }
 
-$(document).keydown(onKeyDown);
-$(document).keyup(onKeyUp);
+function preveriZmago() {
+    if (!gameWon && allBricksDestroyed()) {
+        gameWon = true;
+        clearInterval(timerId);
+        clearInterval(intervalId);
+        Swal.fire({
+            title: 'Bravo!',
+            text: 'Koncal si level ' + level,
+            icon: 'success',
+            confirmButtonText: 'V redu'
+        }).then(function () {
+            level++;
+            gameWon = false;
+            updateLevelDisplay();
+            resetBall();
+            init_paddle();
+            initbricks();
+            intervalId = setInterval(draw, 10);
+            timerId = setInterval(timer, 1000);
+        });
+    }
+}
+
 
 function draw() {
+    $(document).keydown(onKeyDown);
+    $(document).keyup(onKeyUp);
     clear();
     ctx.fillStyle = ballcolor;
     circle(x, y, 10);
@@ -169,9 +244,10 @@ function draw() {
 
     if (y < NROWS * rowheight && row >= 0 && col >= 0 && col < NCOLS && bricks[row][col] > 0) {
         dy = -dy;
-        bricks[row][col] --;
-        tocke ++;
+        bricks[row][col]--;
+        tocke++;
         $("#tocke").html(tocke);
+        preveriZmago();
     }
 
     if (x + dx > WIDTH - r || x + dx < 0 + r) {
@@ -194,4 +270,6 @@ function draw() {
 
     x += dx;
     y += dy;
+
+
 }
